9975610c1fae42f584d6ed08256ef809d1dfb612
carmen-tm <carmentorremolina@gmail.com>
2019-05-23 11:43:39 +0200

    Add filters logic and rendering, but having a bug on the check behaviour

--- src/App.js
+++ src/App.js
@@ -12,34 +12,113 @@ class App extends React.Component {
 				dataArr: [],
 				//isFetching is for the Loading
 				isFetching: true
+			},
+			filters: {
+				allGenders: [],
+				allCities: [],
+				//Array because we can have multiple filters (accumulate them!) at the same time, they are not exclusive. Should be the same at the name attribute on the inputs checkbox.
+
+				gendersSelected: [],
+				citiesSelected: []
 			}
 		};
+
+		//Bind all my methods
+		this.handleGenderFilter = this.handleGenderFilter.bind(this);
+		this.handleCityFilter = this.handleCityFilter.bind(this);
 	}
 
 	//Methods
 	getPeople() {
 		fetchPeople().then(data => {
-			this.setState({
-				people: {
-					// dataArr: data.results,
+			this.setState(prevState => {
+				return {
+					people: {
+						// dataArr: data.results,
 
-					//Gonna add a new key on the object with the full name, and another one for the id, as it has not
-					dataArr: data.results.map((item, index) => {
-						return {
-							...item,
-							id: `person-${index}`,
-							fullName: `${item.name.first} ${item.name.last}`
-						};
-					}),
-					//isFetching is for the Loading
-					isFetching: false
-				}
+						//Gonna add a new key on the object with the full name, and another one for the id, as it has not
+						dataArr: data.results.map((item, index) => {
+							return {
+								...item,
+								id: `person-${index}`,
+								fullName: `${item.name.first} ${item.name.last}`
+							};
+						}),
+						//isFetching is for the Loading
+						isFetching: false
+					},
+					filters: {
+						...prevState.filters,
+						allCities: data.results
+							.map(item => {
+								return item.location.city;
+							})
+							//Plus filter to delete duplicates
+							.filter((item, ind, array) => array.indexOf(item) === ind),
+
+						allGenders: data.results
+							//I have all items here
+							.map(item => {
+								return item.gender;
+							})
+							//To delete duplicates, I can filter.
+							//filter by a function that compares the first index of that value (IndexOf) with the actual ind (maybe Granada is twice..)
+							.filter((item, ind, array) => array.indexOf(item) === ind)
+					}
+				};
 			});
+		});
+	}
+
+	handleGenderFilter(event) {
+		const { value, checked } = event.currentTarget;
+		//!!checked gives the opposite of what u see>> gives the intention. If it was checked, the user want to uncheck, so gives a FALSE.
+		console.log(value, checked);
+
+		this.setState(prevState => {
+			return {
+				filters: {
+					...prevState.filters,
+
+					//If it is checked
+					gendersSelected: checked
+						? //checkeo
+						  prevState.filters.gendersSelected.concat(value)
+						: //deschekeo
+						  prevState.filters.gendersSelected.filter(item => item !== value)
+					// genders: prevState.filters.genders.push
+				}
+			};
+		});
+	}
+
+	handleCityFilter(event) {
+		const { value, checked } = event.currentTarget;
+		console.log(value, checked);
+
+		this.setState(prevState => {
+			return {
+				filters: {
+					...prevState.filters,
+					//Check if it is in the array
+					citiesSelected: prevState.filters.citiesSelected.find(
+						item => item === value
+					)
+						? prevState.filters.citiesSelected.filter(item => item !== value)
+						: prevState.filters.citiesSelected.concat(value)
+				}
+			};
 		});
 	}
 
 	render() {
 		const { dataArr, isFetching } = this.state.people;
+		const {
+			allGenders,
+			allCities,
+			gendersSelected,
+			citiesSelected
+		} = this.state.filters;
 		return (
 			<div className="App">
 				<header>
@@ -52,7 +131,14 @@ class App extends React.Component {
 					</main>
 				) : (
 					<main>
-						<FilterList />
+						<FilterList
+							onGenderChange={this.handleGenderFilter}
+							onCityChange={this.handleCityFilter}
+							stateAllGenders={allGenders}
+							stateAllCities={allCities}
+							stateFilterGenders={gendersSelected}
+							stateFilterCities={citiesSelected}
+						/>
 						<PeopleList people={dataArr} />
 					</main>
 				)}
--- src/components/FiltersList/Index.js
+++ src/components/FiltersList/Index.js
@@ -1,13 +1,100 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Styles.scss';
 
 //simple functional component: sfc
-const FilterList = () => {
+const FilterList = props => {
+	console.log(props);
+	const {
+		onGenderChange,
+		onCityChange,
+		stateAllGenders,
+		stateAllCities,
+		stateFilterGenders,
+		stateFilterCities
+	} = props;
 	return (
 		<section className="filters-list">
 			<h2>Lista de filtros</h2>
+			<form>
+				<fieldset>
+					<legend>Genders</legend>
+					{/* <Input
+                            name="female"
+                            onChange={handleOnClick}
+                            checked={true}
+                        /> */}
+					<div>
+						<label htmlFor="female">Female</label>
+						<input
+							type="checkbox"
+							//
+							name="genders"
+							id="female"
+							value="female"
+							onChange={onGenderChange}
+							//We want the checkbox to be controlled component. On inputs texts, that is the value. Here, is if it is checked or not
+							checked={stateFilterGenders.includes('female')}
+						/>
+					</div>
+
+					<div>
+						<label htmlFor="male">Male</label>
+						<input
+							type="checkbox"
+							name="genders"
+							id="male"
+							value="male"
+							onChange={onGenderChange}
+							checked={stateFilterGenders.includes('male')}
+						/>
+					</div>
+
+					<div>
+						<label htmlFor="other">Other</label>
+						<input
+							type="checkbox"
+							name="genders"
+							id="other"
+							value="other"
+							onChange={onGenderChange}
+							checked={stateFilterGenders.includes('other')}
+						/>
+					</div>
+				</fieldset>
+
+				<fieldset>
+					<legend>Cities</legend>
+					{stateAllCities.map((city, index) => {
+						return (
+							<div key={index}>
+								<label htmlFor={city}>{city}</label>
+								<input
+									type="checkbox"
+									name="cities"
+									id={city}
+									value={city}
+									onChange={onCityChange}
+									checked={stateFilterCities.includes({ city })}
+								/>
+							</div>
+						);
+					})}
+				</fieldset>
+			</form>
 		</section>
 	);
+};
+
+FilterList.propTypes = {
+	onGenderChange: PropTypes.func.isRequired,
+	onCityChange: PropTypes.func.isRequired,
+
+	stateAllGenders: PropTypes.arrayOf(PropTypes.string).isRequired,
+	stateAllCities: PropTypes.arrayOf(PropTypes.string).isRequired,
+
+	stateFilterGenders: PropTypes.arrayOf(PropTypes.string).isRequired,
+	stateFilterCities: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default FilterList;
